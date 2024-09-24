cf9974f0a2acece68f556ae3cb46c85c0ff9b48a
Putawan Pao <pardis120@outlook.com>
2024-09-25 01:45:03 +0700

    save

--- games/ภูตะวัน_จุลน้อย.cpp
+++ games/ภูตะวัน_จุลน้อย.cpp
@@ -1,103 +1,128 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <iomanip>
 using namespace std;
 int calscore(int midterm, int final, int homework, int assingnment, int classroom);
 string calculatetotalscore(int sum);
 int main()
 {
-    int score = 0, midterm, final, homework, assingnment, classroom;
-    string name;
+    srand(time(0));
+    int score = 0; //,midterm,final,assingnment,homework,classroom;
+    int time;
     cout << endl;
     system("CLS");
     cout << " ----------------------------------------" << endl;
     cout << "         Program Calculate Grade         " << endl;
     cout << " ----------------------------------------" << endl;
     cout << endl;
-    cout << "Please enter your name : ";
-    getline(cin, name);
+    cout << "Please enter quantity of student : ";
+    cin >> time;
     cout << endl;
-    while (true)
-    {
-        cout << "Enter midterm score (0 - 30 Point) : ";
-        cin >> midterm;
-        cout << endl;
-        if (midterm < 0 || midterm > 30)
-        {
-            cout << "The score entered is incorrect. Plesase enter score (0 - 30 Point)" << endl;
-            continue;
-        }
-        break;
-    }
-    while (true)
-    {
-        cout << "Enter final score (0 - 30 Point) : ";
-        cin >> final;
-        cout << endl;
-        if (final < 0 || final > 30)
-        {
-            cout << "The score entered is incorrect. Plesase enter score (0 - 30 Point)" << endl;
-            continue;
-        }
-        break;
-    }
-    while (true)
-    {
 
-        cout << "Enter homework score (0 - 15 Point) : ";
-        cin >> homework;
-        cout << endl;
-        if (homework < 0 || homework > 15)
-        {
-            cout << "The score entered is incorrect. Plesase enter score (0 - 15 Point)" << endl;
-            continue;
-        }
-        break;
-    }
-
-    while (true)
+    string personNames[50] = {
+        "John", "Jane", "Alice", "Supra", "Charlie", "David", "Emily", "Yuumi", "Grace", "Parker",
+        "Sangsom", "Phupha", "Katarina", "Yasuo", "Grace", "Putawan", "Thanapon", "Paul", "Poonpon", "Kasapat",
+        "Steve", "Tina", "Patrick", "Victor", "Wendy", "Xander", "Lee", "Thanatorn", "Amy", "Ben",
+        "Catherine", "Dylan", "Eva", "Felix", "Peter", "Helen", "Vayne", "Julie", "Kevin", "Laura",
+        "Lauren", "Nika", "Oscar", "Ahri", "Quinn", "Riley", "Sarah", "Tom", "Darius", "Vince"};
+    for (int i = 0; i < time; i++)
     {
+        int final = rand() % 31;
+        int midterm = rand() % 31;
+        int assingment = rand() % 16;
+        int classroom = rand() % 11;
+        int homework = rand() % 16;
 
-        cout << "Enter affective score (0 - 10 Point) : ";
-        cin >> classroom;
-        cout << endl;
-        if (classroom < 0 || classroom > 10)
+        while (true)
         {
-            cout << "The score entered is incorrect. Plesase enter score (0 - 10 Point)" << endl;
-            continue;
+
+            cout << "| Name (Name and Surname): " << personNames[rand() % 50] + " " + personNames[rand() % 50] << endl;
+            cout << "| Midterm score (0 - 30 Point) : ";
+            cout << midterm;
+            cout << endl;
+            if (midterm < 0 || midterm > 30)
+            {
+                cout << "The score entered is incorrect. Plesase enter score (0 - 30 Point)" << endl;
+                midterm = rand() % 31;
+                continue;
+            }
+            break;
         }
-        break;
-    }
-
-    while (true)
-    {
-
-        cout << "Enter report score (0 - 15 Point) : ";
-        cin >> assingnment;
-        cout << endl;
-        if (assingnment < 0 || assingnment > 15)
+        while (true)
         {
-            cout << "The score entered is incorrect. Plesase enter score (0 - 15 Point)" << endl;
-            continue;
+            cout << "| Final score (0 - 30 Point) : ";
+            cout << final;
+            cout << endl;
+            if (final < 0 || final > 30)
+            {
+                cout << "The score entered is incorrect. Plesase enter score (0 - 30 Point)" << endl;
+                int final = rand() % 31;
+                continue;
+            }
+            break;
         }
-        break;
-    }
-    while (true)
-    {
-        if (score > 100)
+        while (true)
         {
-            cout << "!!! score exceeds 100. Please enter valid score again. !!!" << endl;
-            continue;
-        }
-        break;
-    }
-    int scoref = calscore(midterm, final, homework, assingnment, classroom);
-    string grade = calculatetotalscore(scoref);
 
-    cout << "Your total score is " << scoref << endl;
-    cout << endl;
-    cout << "Your grade is " << grade << endl;
+            cout << "| Homework score (0 - 15 Point) : ";
+            cout << homework;
+            cout << endl;
+            if (homework < 0 || homework > 15)
+            {
+                cout << "The score entered is incorrect. Plesase enter score (0 - 15 Point)" << endl;
+                assingment = rand() % 16;
+                continue;
+            }
+            break;
+        }
+
+        while (true)
+        {
+
+            cout << "| Affective score (0 - 10 Point) : ";
+            cout << classroom;
+            cout << endl;
+            if (classroom < 0 || classroom > 10)
+            {
+                cout << "The score entered is incorrect. Plesase enter score (0 - 10 Point)" << endl;
+                classroom = rand() % 11;
+                continue;
+            }
+            break;
+        }
+
+        while (true)
+        {
+
+            cout << "| Report score (0 - 15 Point) : ";
+            cout << assingment;
+            cout << endl;
+            if (assingment < 0 || assingment > 15)
+            {
+                cout << "The score entered is incorrect. Plesase enter score (0 - 15 Point)" << endl;
+                homework = rand() % 16;
+                continue;
+            }
+            break;
+        }
+        while (true)
+        {
+            if (score > 100)
+            {
+                cout << "!!! score exceeds 100. Please enter valid score again. !!!" << endl;
+                continue;
+            }
+            break;
+        }
+        int scoref = calscore(midterm, final, homework, assingment, classroom);
+        string grade = calculatetotalscore(scoref);
+        cout << "| Student " << i + 1 << " total score is " << scoref << " get grade : " << grade << endl;
+        cout << "----------------------------------------------------" << endl;
+    }
 }
-
 int calscore(int midterm, int final, int homework, int assingnment, int classroom)
 {
     int sum = midterm + final + homework + assingnment + classroom;
@@ -109,7 +134,7 @@ string calculatetotalscore(int sum)
 
     string grade;
 
-    if (sum >= 80)
+    if (sum >= 76)
     {
         grade = "A";
     }
